a826d5e00c1bfdca2143b2d92b3d62607b9cd7d1
fazia91 <faziaakouche6@gmail.com>
2020-08-23 15:48:36 -0700

    first commit

---
+++ ExpediaFramework/src/main/java/bootCamp/ExpediaFramework/App.java
@@ -0,0 +1,13 @@
+package bootCamp.ExpediaFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
